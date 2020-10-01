d491a6dc0d2c911bea98e8773237b66b651f6f6d
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-10-01 18:38:15 -0400

    imitial upload of files to GitHub

---
+++ main.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+// prompt to ask how many games to play//
+
+var games = prompt("How many rounds would you like to play?", "1 to 9");
+
+
+
+
+
+
+
+
+/* Outocmes done in if statements are as follows:
+	rock beats scissors
+	paper beats rock
+	scissor beats paper
+	if same it is a tie*/
+	
+	
